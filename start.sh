7e594628123c919bb7809658cafbbef57a9bfac0
Anapopo <favoursmoe@gmail.com>
2017-09-16 00:03:52 +0800

    添加编译脚本

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+bin="LEDMatrix";
+
+if [ -f "$bin" ]; then
+  rm $bin
+  echo "Cleanup old files..."
+fi
+echo "Compiling..."
+g++ LEDMatrix.cpp -o $bin -std=c++11 -lwiringPi -lm
+echo "Starting..."
+sudo ./$bin
